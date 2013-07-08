7d8182d31a7cba9465b8fe5fe4750e33c19e61e1
Don Nguyen <you@example.com>
2013-07-08 23:18:21 +1000

    init

---
+++ web.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var array = fs.readFileSync('index.html').toString().split("\n");
+var express = require('express');
+var app = express.createServer(express.logger());
+app.get('/', function(request, response) {
+  response.send(array[0]);
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
