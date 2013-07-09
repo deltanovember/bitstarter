c2c29d6e8614c22ab56efeff9220c74cf1da31f9
Don Nguyen <you@example.com>
2013-07-09 16:09:56 +1000

    ex3

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var fs = require('fs');
 var array = fs.readFileSync('index.html').toString().split("\n");
 var express = require('express');
 var app = express.createServer(express.logger());
-app.get('/', function(request, response) {
-  response.send(array[0]);
+app.get('/', function(req, res) {
+  fs.readFile('./index.html', function(error, content) {
+    if (error) {
+      res.writeHead(500);
+      res.end();
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
